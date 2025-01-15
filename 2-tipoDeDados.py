df3e570a63bc79348dc4d8428dcd224016207dd3
VianaMarcioDev <marcioviana.developer@gmail.com>
2025-01-14 21:29:02 -0300

    Learning about data types in Python

---
+++ 2-tipoDeDados.py
@@ -0,0 +1,14 @@
+name = "Euro Truck Simulator 2"
+yearLaunch = 2022
+gamePrice = 199.00
+planIncluded = False
+
+print(name)
+print(yearLaunch)
+print(gamePrice)
+print(planIncluded)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(gamePrice))
+print(type(planIncluded))
