56e900921ce81f5676b0eb31565b2af014264692
VianaMarcioDev <marcioviana.developer@gmail.com>
2025-02-01 21:27:12 -0300

    Aprendendo sobre operadores aritméticos e relacionais

---
+++ 4-operadoresAritmeticosERelacionais.py
@@ -0,0 +1,32 @@
+print("testando " + "concatenação " + "com sinal de +")
+
+num1 = 1
+num2 = 1
+
+# Operadores aritméticos
+
+sum = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+mod = num1 % num2
+exp = num1 ** num2
+
+# Comparação
+
+bigger = num1 > num2
+smaller = num1 < num2
+equal = num1 == num2
+bigger_equal = num1 >= num2
+smaller_equal = num1 <= num2
+
+# Atribuição
+
+num1 = 31
+num1 += 1
+num1 -= 1
+num1 *= 1
+num1 /= 1
+num1 %= 1
+num1 **= 1
+
